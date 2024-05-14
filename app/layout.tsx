76dcc2213bdec8d6bb6433f23adcb04c7ff9fc1f
atik-persei <atikpersei@gmail.com>
2024-05-14 13:52:53 +0000

    ✨ feat: Apply Noto_Sans_KR font across layout.tsx
    - Set Noto_Sans_KR as the default font in layout.tsx to ensure uniform typography across the component.
    - Updated CSS properties to include Noto_Sans_KR, enhancing readability and visual coherence.

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,12 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Noto_Sans_KR } from "next/font/google";
+
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const noto = Noto_Sans_KR({
+  weight: '400',
+  subsets: ['latin'],
+})
 
 export const metadata: Metadata = {
   title: "컴퍼레이",
@@ -16,7 +20,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="kr">
-      <body className={inter.className}>{children}</body>
+      <body className={noto.className}>{children}</body>
     </html>
   );
 }
