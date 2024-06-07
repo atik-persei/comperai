fb0631f974d4a13ec962aa2bb495504345c7d0fc
atik-persei <atikpersei@gmail.com>
2024-06-07 13:58:54 +0000

    ♻️ refactory: Update imports and text in limit/page.tsx
    - Change import paths to use absolute paths for consistency
    - Update 'Render' component import to 'MainRender'

--- app/limit/page.tsx
+++ app/limit/page.tsx
@@ -1,12 +1,12 @@
-import Header from "../components/Header";
-import Render from "../components/Render";
-import Footer from "../components/Footer";
+import Header from '@/app/components/Header';
+import Render from '@/app/components/MainRender';
+import Footer from '@/app/components/Footer';
 
 export default function Page() {
   return (
     <>
       <Header></Header>
-      <Render text="요청 제한을 초과했습니다."></Render>
+      <Render text='요청 제한을 초과했습니다.'></Render>
       <Footer></Footer>
     </>
   );
