4c1ac2120a9bcaa42131fc84239684e5fb5ed552
atik-persei <atikpersei@gmail.com>
2024-06-08 11:09:51 +0000

    ♻️ refactory: Update Docs component structure and imports
    - Change import paths to use absolute paths for consistency
    - Add optional 'sidetype' prop to conditionally render 'Structure' and 'Navigation' components
    - Import necessary CSS for 'katex' and 'prismjs' styling

--- app/components/Docs.tsx
+++ app/components/Docs.tsx
@@ -1,26 +1,31 @@
-"use client";
+import React, { ReactNode } from 'react';
 
-import React, { ReactNode, useEffect } from "react";
-import Structure from "@/app/components/DocsStructure";
-import Prose from "@/app/components/DocsProse";
-import Navigation from "@/app/components/DocsNavigation";
-import { Mdx, TreeNode } from "@/app/types/mdx";
+import Structure from '@/app/components/DocsStructure';
+import Prose from '@/app/components/DocsProse';
+import Navigation from '@/app/components/DocsNavigation';
+
+import { Mdx, TreeNode } from '@/app/types/mdx';
+
+import 'katex/dist/katex.min.css';
+import 'prismjs/themes/prism.css';
+
 
 interface DocsProps {
   children: ReactNode;
   contents: Mdx[];
   structre: TreeNode[];
+  sidetype?: boolean;
 }
 
-const Docs: React.FC<DocsProps> = ({ children, contents, structre }) => {
+const Docs: React.FC<DocsProps> = ({ children, contents, structre, sidetype=true }) => {
   return (
-    <main className="bg-white">
-      <div className="relative max-w-screen-xl px-4 py-10 mx-auto md:flex md:flex-row md:py-10">
-        <Structure structre={structre}></Structure>
+    <main className='bg-white'>
+      <div className='relative max-w-screen-xl px-4 py-10 mx-auto md:flex md:flex-row md:py-10'>
+        { sidetype ? <Structure structre={structre}></Structure> : '' }
         <Prose>
           {children}
         </Prose>
-        <Navigation contents={contents}></Navigation>
+        { sidetype ? <Navigation contents={contents}></Navigation> : '' }
       </div>
     </main>
   );
