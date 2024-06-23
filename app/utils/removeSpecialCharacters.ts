93b1424d1d5acef5bba95db362b1c63cfd725058
atik-persei <atikpersei@gmail.com>
2024-06-23 14:19:15 +0000

    🔨 chore: Update removeSpecialCharacters to convert text to lowercase
    - Modify 'removeSpecialCharacters' function to convert input text to lowercase
    - Ensure consistent formatting by replacing spaces and special characters

--- app/utils/removeSpecialCharacters.ts
+++ app/utils/removeSpecialCharacters.ts
@@ -2,5 +2,5 @@ export default function removeSpecialCharacters(text: string | undefined): strin
   if (typeof text != "string") {
     return ""
   }
-  return text.replaceAll(" ", "-").replaceAll("%20", "-").replaceAll(/[^a-zA-Z0-9가-힣 _-]/g, '');
+  return text.toLowerCase().replaceAll(" ", "-").replaceAll("%20", "-").replaceAll(/[^a-zA-Z0-9가-힣 _-]/g, '');
 }
