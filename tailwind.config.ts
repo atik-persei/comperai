7f49113cdd8cf67ec21b0f5fd31f4287dc84344e
atik-persei <atikpersei@gmail.com>
2024-06-26 08:57:55 +0000

    🔧 config: Update Tailwind CSS for new MDX components and plugins
    - Add new file paths to the 'content' array in 'tailwind.config.ts'
      - Include 'mdx-components.tsx'
      - Include 'rehype-filename-plugin.mjs'
      - Include 'rehype-table-plugin.mjs'
    - Add default safelist items to Tailwind CSS configuration

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,12 +2,23 @@ import type { Config } from "tailwindcss";
 
 const config: Config = {
   content: [
+    "./mdx-components.tsx",
+    "./rehype-filename-plugin.mjs",
+    "./rehype-table-plugin.mjs",
     "./pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./components/**/*.{js,ts,jsx,tsx,mdx}",
     "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
+      spacing: {
+        "15": "3.75rem",     // 60px
+        "50": "12.5rem",     // 200px
+        "71": "17.75rem",    // 284px
+        "87.5": "21.875rem", // 350px
+        "88": "22rem",       // 360px
+        "150": "37.5rem"     // 600px
+      },
       backgroundImage: {
         "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
         "gradient-conic":
@@ -16,5 +27,10 @@ const config: Config = {
     },
   },
   plugins: [],
+  safelist: [
+    {
+      pattern: /^(min|max|w|h|text)-/
+    },
+  ],
 };
 export default config;
