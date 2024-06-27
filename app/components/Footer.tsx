bc3d06bb09eb517752db23a971d3b3ce19f5260c
atik-persei <atikpersei@gmail.com>
2024-06-27 22:27:50 +0000

    ✨ feat: Use CreateArrayLink in Header and Footer component
    - Modify links in the 'Footer' component to point to correct routes
    - Ensure consistent rendering of footer links using 'CreateArrayLink'

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -1,24 +1,21 @@
 import Image from "next/image";
 import { v4 as uuidv4 } from "uuid";
+import CreateArrayLink from "@/app/utils/createArrayLink";
 
 const navigationService = [
   { name: "서비스", description: "", href: "#" },
-  { name: "공지사항", description: "", href: "#" },
-  { name: "자주 묻는 질문", description: "", href: "#" },
-  { name: "서버 상태", description: "", href: "#" },
+  { name: "공지사항", description: "", href: "/notification" },
+  { name: "자주 묻는 질문", description: "", href: "/faq" },
 ];
 
 const navigationLegal = [
   { name: "법적 고지", description: "", href: "#" },
-  { name: "서비스 이용약관", description: "", href: "#" },
-  { name: "개인정보처리방침", description: "", href: "#" },
-  { name: "라이선스", description: "", href: "#" },
+  { name: "서비스 이용약관", description: "", href: "/terms" },
 ];
 
 const navigationSupport = [
   { name: "지원", description: "", href: "#" },
-  { name: "고객센터", description: "", href: "#" },
-  { name: "이메일", description: "", href: "#" },
+  { name: "고객센터", description: "", href: "https://github.com/atik-persei/comperai/issues" },
 ];
 
 const navigationPotal = [
@@ -27,64 +24,36 @@ const navigationPotal = [
 
 export default function Footer() {
   return (
-    <footer className="relative mx-auto flex h-fit min-w-[360px] flex-col items-center justify-center gap-y-5 bg-[#f2f2f2] pt-[60px] pb-[60px] text-zinc-500">
+    <footer className="relative flex flex-col items-center justify-center mx-auto bg-gray-100 h-fit min-w-88 gap-y-5 pt-15 pb-15 text-zinc-500">
       {/* 상단 네비게이션 */}
-      <section className="mb-24 flex flex-row justify-between w-full max-w-4xl text-[14px]">
-        <div className="flex flex-col">
-          {navigationService.map((item, index) => {
-            if (index == 0) {
-              return (
-                <span className="font-bold" key={uuidv4()}>{item.name}</span>
-              );
-            }
-            return (
-              <a href={item.href} key={uuidv4()}>{item.name}</a>
-            );
-          })}
+      <section className="flex flex-row justify-between w-full max-w-4xl mb-20 text-sm">
+        <div className="flex flex-col gap-2">
+          {navigationService.map((item, index) => (
+            <CreateArrayLink menu={item} index={index} key={uuidv4()}></CreateArrayLink>
+          ) )}
         </div>
 
-        <div className="flex flex-col">
-          {navigationLegal.map((item, index) => {
-            if (index == 0) {
-              return (
-                <span className="font-bold" key={uuidv4()}>{item.name}</span>
-              );
-            }
-            return (
-              <a href={item.href} key={uuidv4()}>{item.name}</a>
-            );
-          })}
+        <div className="flex flex-col gap-2">
+          {navigationLegal.map((item, index) => (
+            <CreateArrayLink menu={item} index={index} key={uuidv4()}></CreateArrayLink>
+          ) )}
         </div>
 
-        <div className="flex flex-col">
-          {navigationSupport.map((item, index) => {
-            if (index == 0) {
-              return (
-                <span className="font-bold" key={uuidv4()}>{item.name}</span>
-              );
-            }
-            return (
-              <a href={item.href} key={uuidv4()}>{item.name}</a>
-            );
-          })}
+        <div className="flex flex-col gap-2">
+          {navigationSupport.map((item, index) => (
+            <CreateArrayLink menu={item} index={index} key={uuidv4()}></CreateArrayLink>
+          ) )}
         </div>
 
-        <div className="flex flex-col">
-          {navigationPotal.map((item, index) => {
-            if (index == 0) {
-              return (
-                <span className="font-bold" key={uuidv4()}>{item.name}</span>
-              );
-            }
-            return (
-              <a href={item.href} key={uuidv4()}>{item.name}</a>
-            );
-          })}
+        <div className="flex flex-col gap-2">
+          {navigationPotal.map((item, index) => (
+            <CreateArrayLink menu={item} index={index} key={uuidv4()}></CreateArrayLink>
+          ) )}
         </div>
       </section>
 
       {/* 하단 네비게이션 */}
-      <section className="flex flex-row items-center justify-between w-full max-w-4xl text-[14px]">
+      <section className="flex flex-row items-center justify-between w-full max-w-4xl text-sm">
         <div className="flex flex-col gap-1">
           <h3 className="text-base font-bold">컴퍼레이 | COMPERAI</h3>
           <p>서비스 상태에 따라 지연될 수 있습니다.</p>
--- app/components/Header.tsx
+++ app/components/Header.tsx
@@ -3,23 +3,23 @@ import { v4 as uuidv4 } from "uuid";
 
 const menus = [
   { name: "홈", href: "/" },
-  { name: "커밋", href: "/commit" },
+  { name: "문서", href: "/docs" },
 ];
 
 export default function Header() {
   return (
-    <header className="bg-white">
-      <nav className="mx-auto flex max-w-7xl items-center justify-between p-4 lg:px-8" aria-label="Global">
-        <div className="flex lg:flex-1 gap-8">
+    <header className="sticky top-0 p-2 bg-white z-50">
+      <nav className="flex items-center justify-between mx-auto bg-white max-w-7xl lg:2" aria-label="Global">
+        <div className="flex gap-8 lg:flex-1">
           <a href="#" className="flex flex-row items-center justify-center pointer-events-none select-none">
             <Image src="/logo.png" height={40} width={40} alt="" priority={true} />
-            <span className="text-lg sm:text-xl font-bold">COMPERAI</span>
+            <span className="text-lg font-bold sm:text-xl">COMPERAI</span>
           </a>
 
-          <div className="flex gap-4 max-w-7xl items-center justify-between">
+          <div className="flex items-center justify-between gap-4 max-w-7xl">
             {menus.map((item, index) => {
               return (
-                <a href={item.href} key={uuidv4()}>{item.name}</a>
+                <a className="font-medium" href={item.href} key={uuidv4()}>{item.name}</a>
               )
             })}
           </div>
